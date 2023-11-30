ab4d62efa2dad5294205ac96f6a5765c1e4408d7
Valentina Rodriguez Henao <144085605+valtinarh@users.noreply.github.com>
2023-11-30 01:28:11 -0500

    Actividad 6

---
+++ Friends/src/main/java/actividad6/friends/Friends.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package actividad6.friends;
+
+/**
+ *
+ * @author varoh
+ */
+public class Friends {
+
+    public static void main(String[] args) {
+        Ventana ventana = new Ventana();
+        ventana.setVisible(true);
+    }
+}
